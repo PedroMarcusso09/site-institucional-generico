0a77f88bbe1a0278750db53edcae33ad6f958a96
Pedro <pedromarcusso@gmail.com.br>
2023-04-02 14:43:36 -0300

    entrega realizada

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,24 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal(){
+    const button = document.querySelector("#header__button");
+    const modal = document.querySelector('#modal__container')
+
+    button.addEventListener('click', () => {
+        
+        modal.showModal()
+        closeModal()
+        
+    })
+
+    
+}
+
+function closeModal(){
+    const button = document.querySelector('#close__modal')
+    const modal = document.querySelector('#modal__container')
+
+    button.addEventListener('click', () => {
+        modal.close()
+    })
+}
+handleModal()
+
